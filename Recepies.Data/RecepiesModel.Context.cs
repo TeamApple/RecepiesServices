4573bf454e2fef7b5bd875aebaf8c3f5da80e5cc
venelin-p-petrov <venelin.p.petrov@gmail.com>
2013-09-15 15:53:09 +0300

    Fixed Context name

--- Recepies.Data/RecepiesModel.Context.cs
+++ Recepies.Data/RecepiesModel.Context.cs
@@ -13,10 +13,10 @@ namespace Recepies.Data
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class dbcc87f75bab1e4f11b506a23a00be151aEntities : DbContext
+    public partial class RecipeContext : DbContext
     {
-        public dbcc87f75bab1e4f11b506a23a00be151aEntities()
-            : base("name=dbcc87f75bab1e4f11b506a23a00be151aEntities")
+        public RecipeContext()
+            : base("name=RecipeEntities")
         {
         }
     
