2da952098bc9ed35be7c0b625090e85b390c99ea
fanni-p <fanni@abv.bg>
2013-09-15 16:15:24 +0300

    Add UsersController

--- Recepies.Services/App_Start/WebApiConfig.cs
+++ Recepies.Services/App_Start/WebApiConfig.cs
@@ -10,6 +10,16 @@ namespace Recepies.Services
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+                name: "UserLoginApi",
+                routeTemplate: "api/auth/token",
+                defaults: new { controller = "users", action = "token" });
+
+            config.Routes.MapHttpRoute(
+                name: "UserApi",
+                routeTemplate: "api/users/{action}",
+                defaults: new { controller = "users" });
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
